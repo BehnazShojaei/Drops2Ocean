209809e489ba422f6d3816ec432b520bf05df837
Behnaz <behnaz.shojaei88@gmail.com>
2024-12-05 14:39:19 +0800

    update on my homepage

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -6,19 +6,20 @@ import dropimage from "../../assets/drop1.jpeg";
 function HomePage() {
   const { projects, isLoading, error } = useProjects();
 
-  // if (isLoading) {
-  //   return (<p>loading...</p>);
-  // }
+  if (isLoading) {
+    return (<p>loading...</p>);
+  }
 
-  // if (error) {
-  //   return (<p>{error.message}</p>);
-  // }
+  if (error) {
+    return (<p>Error: {error.message}</p>);
+  }
 
   //question if i am using a hook then why i need again to check isloading and error here?
+  //cz the hook is not handling rendering. it only provide data and state
 
 
   return (
-    <div>
+    <>
       {/* Hero Section */}
       <div className="hero-section">
         <img src={dropimage} alt="drop image" className="hero-image" />
@@ -32,11 +33,12 @@ function HomePage() {
       <div id="project-list">
         <h1> Featured projects</h1>
         {/* console.log(projectData); */}
+        {/* render projects */}
         {projects.map((projectData, index) => {
           return <ProjectCard key={index} projectData={projectData} />
         })}
       </div>
-    </div>
+    </>
   );
 }
 
