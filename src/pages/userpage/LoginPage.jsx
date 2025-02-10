a575d81753bd6555df8ae2b31fcd9484ee50eafa
Behnaz <behnaz.shojaei88@gmail.com>
2025-02-10 21:05:13 +0800

    buttons on login fixed, functionality tested

--- src/pages/userpage/LoginPage.jsx
+++ src/pages/userpage/LoginPage.jsx
@@ -12,7 +12,7 @@ function LoginPage() {
     const handleSignUpClick = () => {
         setShowSignUp(!showSignUp);
     };
-    const handleHomePageRedirect = () => {
+    const handleLogin = () => {
         navigate("/homepage", { state: { showButtons: true } }); // Pass state to homepage
     };
 
@@ -23,32 +23,43 @@ function LoginPage() {
 
                 <h2>Login</h2>
                 <p>Willing to create a project or make a pledge? Please login below:</p>
-                <LoginForm />
+
+                <div className="button-container">
+                    <LoginForm onLogin={handleLogin} />
+                    <p>Or</p>
+
+                    <button onClick={handleSignUpClick} className="button secondary">
+                        Create a profile
+                    </button>
+                </div>
+
+
+
+                {/* <LoginForm /> */}
                 {/* <button onClick={() => navigate("/forgot-password")} className="button forgot-password">
                     Forgot Password?
                 </button> */}
 
-                <p>Or</p>
             </div>
 
 
-            <div className="signup-section">
-                {/* Show the button only if the form is not displayed */}
+            {/* <div className="signup-section"> */}
+            {/* Show the button only if the form is not displayed
                 {!showSignUp && (
                     <button onClick={handleSignUpClick} className="button">
                         Create a profile
                     </button>
-                )}
+                )} */}
 
-                {/* Show the SignUpForm if showSignUp is true */}
-                {showSignUp && (
-                    <div className="signup-form">
-                        <h2>Create a profile</h2>
-                        <SignUpForm />
-                    </div>
-                )}
-            </div>
+            {/* Show the SignUpForm if showSignUp is true */}
+            {showSignUp && (
+                <div className="signup-form">
+                    <h2>Create a profile</h2>
+                    <SignUpForm />
+                </div>
+            )}
         </div>
+        // </div>
     );
 }
 
